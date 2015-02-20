8e072f2638683616f206cca3fda46a8fea887d4f
virendram <virendra.mishra@gmail.com>
2015-02-20 00:06:55 -0600

    Added the code to get the price of the house

--- Assignment_1/mlclass-ex1-008/mlclass-ex1-008/mlclass-ex1/ex1_multi.m
+++ Assignment_1/mlclass-ex1-008/mlclass-ex1-008/mlclass-ex1/ex1_multi.m
@@ -84,19 +84,26 @@ X = [ones(m, 1) X];
 fprintf('Running gradient descent ...\n');
 
 % Choose some alpha value
-alpha = 0.01;
+figure;
+% hold on;
+% for alpha = 0.001:0.01:0.1;
+alpha = 0.06;
 num_iters = 400;
 
 % Init Theta and Run Gradient Descent 
 theta = zeros(3, 1);
 [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters);
 
+
 % Plot the convergence graph
-figure;
+
 plot(1:numel(J_history), J_history, '-b', 'LineWidth', 2);
 xlabel('Number of iterations');
 ylabel('Cost J');
-
+% legend('alpha');
+% alpha;
+% pause;
+% end
 % Display gradient descent's result
 fprintf('Theta computed from gradient descent: \n');
 fprintf(' %f \n', theta);
@@ -107,6 +114,9 @@ fprintf('\n');
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
 price = 0; % You should change this
+
+new_X = [1650,3];
+price = [1,(new_X-mu)./sigma]*theta;
 
 
 % ============================================================
@@ -152,6 +162,8 @@ fprintf('\n');
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
 price = 0; % You should change this
+new_X = [1650,3];
+price = [1,(new_X)]*theta;
 
 
 % ============================================================
